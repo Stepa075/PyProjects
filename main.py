60f7f53f156cb0f3c79b1839ab111791c75cc286
Admin <stepa0751@gmail.com>
2022-08-24 13:04:34 +0300

    with server settings

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
-
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = '5768351177:AAHVeN0j7BwQt3qQmKgRlYcTVk9AsCDANbk'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
 # webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_HOST = f'217.160.249.31'
+WEBHOOK_PATH = f'/usr/local/bin/bot'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_PORT = '5000'
 
 
 async def on_startup(dispatcher):
